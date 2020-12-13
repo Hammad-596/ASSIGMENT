2b4773edfa10e40f597d14762804914d468b5605
HAMMAD AHMED <ahmedhammad596@gmail.com>
2020-12-13 17:06:08 +0500

    project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import list from "./list";
 
 function App() {
   return (
     <div className="App">
-      
-      <div> hello world in this web</div> 
+       list = (document.getelementby id(list));
     </div>
   );
 }
---
+++ src/list.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default list = () => {
+    return(
+        form(
+            firstname = ""
+            
+        )
+    );
+};
